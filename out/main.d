2ce8156b62e1f5af37adf6ec219c485ae772bd5f
bilibiliLaiabao55 <bilibili_laiabao@126.com>
2024-10-01 13:59:58 +0800

    Add files via upload

--- out/main.d
+++ out/main.d
@@ -1,12 +1,15 @@
-out/main.o: main.c F:/sgdk/inc/genesis.h F:/sgdk/inc/config.h \
- F:/sgdk/inc/asm.h F:/sgdk/inc/types.h F:/sgdk/inc/sys.h \
- F:/sgdk/inc/sram.h F:/sgdk/inc/mapper.h F:/sgdk/inc/memory.h \
- F:/sgdk/inc/tools.h F:/sgdk/inc/bmp.h F:/sgdk/inc/maths.h \
- F:/sgdk/inc/vdp.h F:/sgdk/inc/pal.h F:/sgdk/inc/dma.h \
- F:/sgdk/inc/vdp_tile.h F:/sgdk/inc/vdp_bg.h F:/sgdk/inc/map.h \
- F:/sgdk/inc/font.h F:/sgdk/inc/string.h F:/sgdk/inc/tab_cnv.h \
- F:/sgdk/inc/maths3D.h F:/sgdk/inc/vdp_dma.h F:/sgdk/inc/vdp_spr.h \
- F:/sgdk/inc/vdp_pal.h F:/sgdk/inc/vram.h F:/sgdk/inc/sprite_eng.h \
- F:/sgdk/inc/sound.h F:/sgdk/inc/xgm.h F:/sgdk/inc/z80_ctrl.h \
- F:/sgdk/inc/ym2612.h F:/sgdk/inc/psg.h F:/sgdk/inc/joy.h \
- F:/sgdk/inc/timer.h resource.h
+out/main.o: main.c G:/sgdk/inc/genesis.h G:/sgdk/inc/types.h \
+ G:/sgdk/inc/config.h G:/sgdk/inc/asm.h G:/sgdk/inc/sys.h \
+ G:/sgdk/inc/sram.h G:/sgdk/inc/mapper.h G:/sgdk/inc/memory.h \
+ G:/sgdk/inc/memory_base.h G:/sgdk/inc/tools.h G:/sgdk/inc/bmp.h \
+ G:/sgdk/inc/maths.h G:/sgdk/inc/vdp.h G:/sgdk/inc/pal.h \
+ G:/sgdk/inc/dma.h G:/sgdk/inc/vdp_tile.h G:/sgdk/inc/vdp_bg.h \
+ G:/sgdk/inc/map.h G:/sgdk/inc/pool.h G:/sgdk/inc/object.h \
+ G:/sgdk/inc/font.h G:/sgdk/inc/string.h G:/sgdk/inc/tab_cnv.h \
+ G:/sgdk/inc/maths3D.h G:/sgdk/inc/vdp_spr.h G:/sgdk/inc/vdp_pal.h \
+ G:/sgdk/inc/vram.h G:/sgdk/inc/sprite_eng.h \
+ G:/sgdk/inc/sprite_eng_legacy.h G:/sgdk/inc/sound.h G:/sgdk/inc/xgm.h \
+ G:/sgdk/inc/z80_ctrl.h G:/sgdk/inc/ym2612.h G:/sgdk/inc/psg.h \
+ G:/sgdk/inc/joy.h G:/sgdk/inc/timer.h G:/sgdk/inc/task.h \
+ G:/sgdk/inc/task_cst.h G:/sgdk/inc/ext/flash-save/flash.h \
+ G:/sgdk/inc/types.h G:/sgdk/inc/ext/flash-save/saveman.h resource.h
--- out/resource.d
+++ out/resource.d
@@ -2,9 +2,12 @@ out/resource.o: resource.res \
 res/IMAGE_LOGO.png \
 res/AUDIO_DRONE.vgm \
 res/battle.vgm \
+res/snd_weaponpull.wav \
 res/spr_kris.png \
 res/battle_back.png \
 res/snd_appear.wav \
 res/spr_heart_intro.png \
+res/snd_battle_start.wav \
 res/legand.vgm \
-res/snd_erun.wav
+res/snd_erun.wav \
+res/snd_impact.wav
