1e0f4708ad3d687b02915d4d6dfa22c88d6d5dca
bilibiliLaiabao55 <bilibili_laiabao@126.com>
2024-10-04 14:05:10 +0800

    Add files via upload

--- out/main.d
+++ out/main.d
@@ -1,15 +1,12 @@
-out/main.o: main.c G:/sgdk/inc/genesis.h G:/sgdk/inc/types.h \
- G:/sgdk/inc/config.h G:/sgdk/inc/asm.h G:/sgdk/inc/sys.h \
+out/main.o: main.c G:/sgdk/inc/genesis.h G:/sgdk/inc/config.h \
+ G:/sgdk/inc/asm.h G:/sgdk/inc/types.h G:/sgdk/inc/sys.h \
  G:/sgdk/inc/sram.h G:/sgdk/inc/mapper.h G:/sgdk/inc/memory.h \
- G:/sgdk/inc/memory_base.h G:/sgdk/inc/tools.h G:/sgdk/inc/bmp.h \
- G:/sgdk/inc/maths.h G:/sgdk/inc/vdp.h G:/sgdk/inc/pal.h \
- G:/sgdk/inc/dma.h G:/sgdk/inc/vdp_tile.h G:/sgdk/inc/vdp_bg.h \
- G:/sgdk/inc/map.h G:/sgdk/inc/pool.h G:/sgdk/inc/object.h \
+ G:/sgdk/inc/tools.h G:/sgdk/inc/bmp.h G:/sgdk/inc/maths.h \
+ G:/sgdk/inc/vdp.h G:/sgdk/inc/pal.h G:/sgdk/inc/dma.h \
+ G:/sgdk/inc/vdp_tile.h G:/sgdk/inc/vdp_bg.h G:/sgdk/inc/map.h \
  G:/sgdk/inc/font.h G:/sgdk/inc/string.h G:/sgdk/inc/tab_cnv.h \
- G:/sgdk/inc/maths3D.h G:/sgdk/inc/vdp_spr.h G:/sgdk/inc/vdp_pal.h \
- G:/sgdk/inc/vram.h G:/sgdk/inc/sprite_eng.h \
- G:/sgdk/inc/sprite_eng_legacy.h G:/sgdk/inc/sound.h G:/sgdk/inc/xgm.h \
- G:/sgdk/inc/z80_ctrl.h G:/sgdk/inc/ym2612.h G:/sgdk/inc/psg.h \
- G:/sgdk/inc/joy.h G:/sgdk/inc/timer.h G:/sgdk/inc/task.h \
- G:/sgdk/inc/task_cst.h G:/sgdk/inc/ext/flash-save/flash.h \
- G:/sgdk/inc/types.h G:/sgdk/inc/ext/flash-save/saveman.h resource.h
+ G:/sgdk/inc/maths3D.h G:/sgdk/inc/vdp_dma.h G:/sgdk/inc/vdp_spr.h \
+ G:/sgdk/inc/vdp_pal.h G:/sgdk/inc/vram.h G:/sgdk/inc/sprite_eng.h \
+ G:/sgdk/inc/sound.h G:/sgdk/inc/xgm.h G:/sgdk/inc/z80_ctrl.h \
+ G:/sgdk/inc/ym2612.h G:/sgdk/inc/psg.h G:/sgdk/inc/joy.h \
+ G:/sgdk/inc/timer.h resource.h
--- out/resource.d
+++ out/resource.d
@@ -1,13 +1,16 @@
 out/resource.o: resource.res \
-res/IMAGE_LOGO.png \
-res/AUDIO_DRONE.vgm \
+res/spr_susie.png \
 res/battle.vgm \
 res/snd_weaponpull.wav \
 res/spr_kris.png \
 res/battle_back.png \
 res/snd_appear.wav \
 res/spr_heart_intro.png \
+res/snd_erun.wav \
+res/snd_impact.wav \
+res/spr_ralsei.png \
+res/IMAGE_LOGO.png \
+res/AUDIO_DRONE.vgm \
 res/snd_battle_start.wav \
 res/legand.vgm \
-res/snd_erun.wav \
-res/snd_impact.wav
+res/bg_myroom.png
